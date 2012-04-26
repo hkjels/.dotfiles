a0a5bcecce7668898388e38f52c42a4f19b1babe
Henrik Kjelsberg <hkjels@me.com>
2012-04-26 17:00:30 +0200

    Added some behavior description
    Mostly jotting here because I'm waiting to take an exam. I hate waiting
    like this!

--- Readme.md
+++ Readme.md
@@ -1,6 +1,17 @@
-# .dotfiles
+# System preferences
 
 __My personal zshell, vim & git preferences__
+
+
+
+## Behavior
+
+I mostly code on OS-X, so these preferences are more optimized for Darwin than
+Linux, but I've taken Linux into concideration. The key-bindings in zsh is set
+up to somewhat mimic vim, but with addition of a few usefull emacs-like
+bindings. There are two dependencies that not yet has found there way into the
+installer. __Excuberant ctags__ & __Ntask__. Installing them should be pretty
+straight forward though.
 
 
 
--- install.sh
+++ install.sh
@@ -8,46 +8,47 @@ DOTFILES=$GITHUB/dotfiles
 
 # Checks
 if [[ $(uname) = 'Linux' ]]; then
-    IS_LINUX=true
-    if [[ -x `which gvim`  ]]; then
-      $HAS_GVIM=true
-    fi
+  IS_LINUX=true
+  if [[ -x `which gvim`  ]]; then
+    $HAS_GVIM=true
+  fi
 fi
 if [[ $(uname) = 'Darwin' ]]; then
-    IS_MAC=true
-    if [[ -x `which mvim`  ]]; then
-      $HAS_MVIM=true
-    fi
+  IS_MAC=true
+  if [[ -x `which mvim`  ]]; then
+    $HAS_MVIM=true
+  fi
 fi
 if [[ -x `which brew` ]]; then
-    alias inst='brew install'
+  alias inst='brew install'
 fi
 if [[ -x `which apt-get` ]]; then
-    alias inst='sudo apt-get install'
+  alias inst='sudo apt-get install'
 fi
 if [[ -x `which yum` ]]; then
-    alias inst='su -c yum install'
+  alias inst='su -c yum install'
+fi
+if [[ -x `which zsh` ]]; then
+  HAS_ZSH=true
 fi
 
 
-# Fetch dependencies
+# Fetch & install dependencies
 git clone git@github.com:hkjels/.dotfiles.git $DOTFILES
 git clone git@github.com:hkjels/zshuery.git $GITHUB/zshuery
 git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
-vim +BundleInstall +qall
 
-# Install zsh if it's not already installed
-if [[ -x `which zsh` ]]; then
+if [[ !$HAS_ZSH ]]; then
   inst zsh
 fi
-
-# Install a graphical vim if it's not already installed
 if [[ $IS_MAC && !$HAS_MVIM ]]; then
   inst mvim
 fi
 if [[ $IS_LINUX && !$HAS_GVIM ]]; then
   inst gvim
 fi
+
+vim +BundleInstall +qall
 
 
 # Link dotfiles to current user
