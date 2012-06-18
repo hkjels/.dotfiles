00f73189c7af839771b6e5fac0e8bb2802663974
Henrik Kjelsberg <hkjels@me.com>
2012-06-18 14:51:50 +0200

    Corrected a typo

--- zsh/autojump.zsh
+++ zsh/autojump.zsh
@@ -4,6 +4,6 @@
 if [ -f `brew --prefix`/etc/autojump ]; then
   . `brew --prefix`/etc/autojump
 elif [ -f /usr/share/autojump/autojump.zsh ]; then
-    . /usr/share/autojump/autojump.zshfi
+    . /usr/share/autojump/autojump.zsh
 fi
 
